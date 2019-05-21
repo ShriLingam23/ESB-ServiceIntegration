755428bed1a6955e373c3f6b393c02c1fbfdc10a
ShriLingam23 <trnshri23@gmail.com>
2019-05-21 20:29:33 +0530

    Back-End Authentication Service modification

--- back_end/Authentication/server.js
+++ back_end/Authentication/server.js
@@ -11,33 +11,33 @@ const clientSecret = 'ced69978c63e88fc96387317742aa8738dbf49df'
 
 const app = express()
 
-// const cors = require('cors');
-// app.use(cors())
 
 // Declare the redirect route
 app.get('/home', (req, res) => {
+
   // The req.query object has the query params that
   // were sent to this route. We want the `code` param
   const requestToken = req.query.code
-  console.log(requestToken)
+
   axios({
-    // make a POST request
+    // Making a POST request
     method: 'post',
-    // to the Github authentication API, with the client ID, client secret
-    // and request token
+
+    // To Github authentication API along with the client ID,secret and request token
     url: `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}`,
-    // Set the content type header, so that we get the response in JSOn
+
+    // Setting the content type header to receive JSON as response
     headers: {
          accept: 'application/json'
     }
   }).then((response) => {
-    // Once we get the response, extract the access token from
-    // the response body
+
+    // Extracting access token from response body
     const accessToken = response.data.access_token
-    console.log(response.data)
-    // redirect the user to the welcome page, along with the access token
+
+    // Redirecting to Home page along with the access token
     res.redirect(`http://localhost:3000/home/${accessToken}`)
-    //res.redirect(`/welcome.html?access_token=${accessToken}`)
+    
   })
 })
 
